c8f06df25a3c7c79fb23fad8c154ec70108ad570
Dmitriy Podorozhnykh <veseliy.koteika@yandex.ru>
2024-12-20 14:21:34 +0300

    fix: dockerfiles & disabled loggers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
-import { DevLogger } from './loggers/devLogger.service';
-import { JsonLogger } from './loggers/jsonLogger.service';
-import { TskvLogger } from './loggers/tskvLogger.service';
+// import { DevLogger } from './loggers/devLogger.service';
+// import { JsonLogger } from './loggers/jsonLogger.service';
+// import { TskvLogger } from './loggers/tskvLogger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -14,17 +14,17 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/afisha');
   app.enableCors();
-  switch (loggerType) {
-    case 'dev':
-      app.useLogger(new DevLogger());
-      break;
-    case 'json':
-      app.useLogger(new JsonLogger());
-      break;
-    case 'tskv':
-      app.useLogger(new TskvLogger());
-      break;
-  }
+  // switch (loggerType) {
+  //   case 'dev':
+  //     app.useLogger(new DevLogger());
+  //     break;
+  //   case 'json':
+  //     app.useLogger(new JsonLogger());
+  //     break;
+  //   case 'tskv':
+  //     app.useLogger(new TskvLogger());
+  //     break;
+  // }
   await app.listen(3000);
 }
 bootstrap();
